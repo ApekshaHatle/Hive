23ae9f721fe07882394cb6191383e21682fcfa49
ApekshaHatle <apekshahatle@gmail.com>
2025-04-21 18:14:26 +0530

    4- profile page and updating user information

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -3,14 +3,17 @@ import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from '@tanstack/react-query';
 import { useEffect } from "react";
 
-const Posts = ({feedType}) => {
-
+const Posts = ({ feedType, username, userId }) => {
 	const getPostEndpoint = () => {
 		switch(feedType) {
 			case "forYou":
 				return "/api/posts/all";
 			case "following":
 				return "/api/posts/following";
+			case "posts":
+				return `/api/posts/user/${username}`;
+			case "likes":
+				return `/api/posts/likes/${userId}`;
 			default:
 				return "/api/posts/all"
 		}
@@ -18,7 +21,7 @@ const Posts = ({feedType}) => {
 
 	const POST_ENDPOINT = getPostEndpoint();
 
-	const {data:posts,isLoading,refetch,isRefetching} = useQuery({
+	const {data:posts, isLoading, refetch, isRefetching} = useQuery({
 		queryKey:["posts"],
 		queryFn: async () => {
 			try {
@@ -36,8 +39,7 @@ const Posts = ({feedType}) => {
 
 	useEffect(() => {
 		refetch();
-
-	}, [feedType, refetch]);
+	}, [feedType, refetch, username]);
 
 	return (
 		<>
